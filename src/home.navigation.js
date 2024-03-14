e4afa388b3b3c0c40254b43765d01d1b3bd30c8d
Subhan Kashif <153631006+SubhanKashif7@users.noreply.github.com>
2024-03-14 16:01:49 +0500

    developed home page

---
+++ src/home.navigation.js
@@ -0,0 +1,16 @@
+const handleNavigation = () => {
+    const nav_anchor = document.querySelectorAll(".nav-anchor");
+nav_anchor.forEach((anchor)=>{
+    anchor.addEventListener("click",(event)=>{
+        if (event.target.id=="address"){
+            window.location.href = "./subpages/address.html"
+        }else if (event.target.id=="contact-us"){
+            window.location.href = "./subpages/contactUs.html"
+        }else if (event.target.id=="complain"){
+            window.location.href = "./subpages/complain.html"
+        }
+    })
+})
+};
+
+window.addEventListener("DOMContentLoaded",handleNavigation)
